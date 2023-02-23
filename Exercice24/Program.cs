cba32d17aca2b2463c7f6577eb0319a8d62d7c93
Pierre Poulin <ppoulin@cegep-ste-foy.qc.ca>
2023-02-23 09:23:43 -0500

    Initial commit

---
+++ Exercice24/Program.cs
@@ -0,0 +1,11 @@
+﻿using Exercice24;
+
+class Program
+{
+  public static void Main(string[] args)
+  {
+	// A COMPLETER
+  }
+
+  // A COMPLETER
+}
---
+++ Exercice24Tests/ProgramTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Exercice24Tests
+{
+  public class ProgramTests
+  {
+    [Fact]
+    public void FONCTION_DESCRIPTION_RESULTAT_ATTENDU ()
+    {
+
+    }
+  }
+}
